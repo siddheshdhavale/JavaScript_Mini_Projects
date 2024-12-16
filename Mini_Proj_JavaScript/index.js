87579d828b3aaa07e14688f472912ea6b5136caa
Siddhesh Dhavale <dhavale.s@northeastern.edu>
2024-12-16 18:12:41 -0500

    Add files via upload

---
+++ Mini_Proj_JavaScript/index.js
@@ -0,0 +1,18 @@
+/*Const var thats not gonna change
+var that is a dynamic var */
+const body = document.getElementsByTagName("body")[0]
+
+function setColor(name) {
+    body.style.backgroundColor = name;
+}
+
+function randomColor() {
+    const red = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+
+    const color = `rgb(${red}, ${blue}, ${green})`  /* Neat String mainpulation*/
+    body.style.backgroundColor = color;
+}
+
+randomColor()
